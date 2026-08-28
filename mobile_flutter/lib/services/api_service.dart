72aaa2423a53cc36be37e17343aeacd9bd948338
Caue51748 <cauebueno51748@gmail.com>
2026-08-28 00:00:09 -0300

    Começo da adição da fotos do drive

--- mobile_flutter/lib/services/api_service.dart
+++ mobile_flutter/lib/services/api_service.dart
@@ -1,5 +1,13 @@
 class ApiService {
-  static const String baseUrl = 'http://143.106.241.11:8080';
+  static const String baseUrl = 'http://localhost:8080';
+
+  //PC do COTIL:
+  //static const String baseUrl = 'http://143.106.241.xx:8080';
+
+  //API Render:
+  //static const String baseUrl = 'https://projeto-integrador-m4jn.onrender.com';
+
+  //Só comentar a primeira linha e descomentar a que for usar
 
   static String usuarios = '$baseUrl/usuarios';
   static String posts = '$baseUrl/posts';
